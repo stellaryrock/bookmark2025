289bd72c97df41fb3d4c641e4929e838f7289d3d
stellaryrock <alohakii@naver.com>
2025-08-18 08:56:16 +0900

    regist validation

--- lib/schema/regist.ts
+++ lib/schema/regist.ts
@@ -2,20 +2,20 @@ import z from 'zod';
 
 const regex = /[~!@#$%^&*()_+_.,]/;
 
-export const passwordSchema = z
+const passwordSchema = z
   .string()
   .min(6, { error: '6자 이상 입력해주세요.' })
   .refine((val) => regex.test(val), {
     error: '특수문자를 하나 이상 포함해주세요.',
   });
 
-export const emailSchema = z.email({ error: '잘못된 이메일 형식입니다.' });
+const emailSchema = z.email({ error: '잘못된 이메일 형식입니다.' });
 
-export const nicknameSchema = z
+const nicknameSchema = z
   .string()
   .min(4, { error: '4자 이상 입력해주세요.' });
 
-export const fileSchema = z
+const fileSchema = z
   .custom<File>((val) => val instanceof File, {
     message: '파일 형식을 확인해주세요.',
   })
@@ -24,7 +24,7 @@ export const fileSchema = z
     'PNG 이미지 파일을 등록해주세요.'
   );
 
-export const authKey = z.number();
+const authKey = z.number();
 
 export const registSchema = z
   .object({
@@ -37,3 +37,7 @@ export const registSchema = z
     ({ password, passwordConfirm }) => password === passwordConfirm,
     '비밀번호가 일치하지 않습니다.'
   );
+  
+  type ValidationError = { errors: string[] } | undefined;
+  export type Regist = z.infer<typeof registSchema>
+  export type RegistError = Partial<Record<keyof Regist, ValidationError>> | undefined;
