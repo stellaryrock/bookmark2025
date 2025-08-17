228c3d500cf815d18db6201c9d160a918af7d8f0
stellaryrock <alohakii@naver.com>
2025-08-17 16:05:37 +0900

    regist form

--- actions/sign.ts
+++ actions/sign.ts
@@ -2,7 +2,9 @@
 
 import { AuthError } from 'next-auth';
 import { v4 as uuidv4 } from 'uuid';
+import z from 'zod';
 import { signIn, signOut } from '@/lib/auth';
+import { registSchema } from '@/lib/schema/regist';
 import { sendRegistCheck } from './mailer';
 
 type Provider = 'google' | 'github' | 'naver' | 'kakao';
@@ -14,13 +16,20 @@ export const login = async (provider: Provider, callback?: string) => {
 };
 
 export const regist = async (formData: FormData) => {
-  const entries = Object.entries(formData);
-  console.log('🚀 ~ entries:', entries);
+  const entries = Object.fromEntries(formData.entries());
 
-  // Todo: zod validate check
-  const authKey = uuidv4();
-  await sendRegistCheck(process.env.google_user!, authKey);
-  console.log('Main has sent.');
+  const result = registSchema.safeParse(entries);
+
+  if (result.success) {
+    const authKey = uuidv4();
+    await sendRegistCheck(process.env.google_user!, authKey);
+    console.log('Mail has sent.');
+  } else {
+    return {
+      success: false,
+      error: z.treeifyError(result.error),
+    };
+  }
 };
 
 // Credential: from login page
--- components/ui/label-input.tsx
+++ components/ui/label-input.tsx
@@ -1,3 +1,4 @@
+import { ValidationError } from '@/app/login/sign-form';
 import { RefObject, useId } from 'react';
 import { cn } from '@/lib/utils';
 import { Input } from './input';
@@ -9,6 +10,7 @@ type Props = {
   defaultValue?: string;
   ref?: RefObject<HTMLInputElement | null>;
   placeholder?: string;
+  validationErrors?: ValidationError;
   className?: string;
 };
 
@@ -19,20 +21,28 @@ export default function LabelInput({
   defaultValue,
   ref,
   placeholder,
+  validationErrors,
   className,
 }: Props) {
   const uniqName = useId();
   return (
-    <label className='text-sm font-semibold capitalize'>
-      {label}
-      <Input
-        name={name || uniqName}
-        type={type || 'text'}
-        ref={ref}
-        defaultValue={defaultValue || ''}
-        placeholder={placeholder}
-        className={cn('bg-gray-100 focus:bg-white font-normal', className)}
-      />
-    </label>
+    <>
+      <label className='text-sm font-semibold capitalize'>
+        {label}
+        <Input
+          name={name || uniqName}
+          type={type || 'text'}
+          ref={ref}
+          defaultValue={defaultValue}
+          placeholder={placeholder}
+          className={cn('bg-gray-100 focus:bg-white font-normal', className)}
+        />
+        {validationErrors?.errors.map((err, idx) => (
+          <p className='text-red-500 font-normal' key={idx}>
+            {err}
+          </p>
+        ))}
+      </label>
+    </>
   );
 }
