a994fc935af4a3021512a3d14fdbfc993f1e2751
stellaryrock <alohakii@naver.com>
2025-08-19 15:20:49 +0900

    login-button

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -2,19 +2,37 @@
 
 import { logout } from '@/actions/sign';
 import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import LabelInput from '@/components/ui/label-input';
+import { useSession } from 'next-auth/react';
 
 export default function My() {
+  const session = useSession();
+  const { name, email, image, isadmin } = session.data.user;
+  console.log(session);
+
   const signOut = async () => {
     await logout();
   };
 
   return (
-    <div className='flex items-center gap-5'>
-      <Button onClick={signOut} variant={'destructive'}>
-        Sign Out
-      </Button>
-      <Link href='/api/auth/signout'>Go LogOut</Link>
+    <div className=''>
+      <div className='flex justify-between items-end p-5 border-b-2'>
+        <h1 className='text-3xl'>
+          내 정보
+        </h1>
+        <button onClick={signOut}>
+          로그아웃
+        </button>
+      </div>
+      <div className="grid grid-cols-2">
+        <div className='left'>
+          asdf
+        </div>
+        <div className='content'>
+          asdf
+        </div>
+      </div>
     </div>
+    
   );
 }
