5d2a2088fed1af82ca8bec275050a30b5e6cf0dc
stellaryrock <alohakii@naver.com>
2025-08-20 09:22:45 +0900

    login-button

--- actions/sign.ts
+++ actions/sign.ts
@@ -14,6 +14,8 @@ import { validate, ValidError, ValidSuccess } from '@/lib/validator';
 
 type Provider = 'google' | 'github' | 'naver' | 'kakao';
 
+export const githubLogin = 
+export const loginNaver = async () => {login('naver')}
 export const login = async (provider: Provider, callback?: string) => {
   await signIn(provider, { redirectTo: callback || '/bookcase' });
 };
