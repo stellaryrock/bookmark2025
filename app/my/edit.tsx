f3bc90c74b565e402620b9bdf18f80215c52cce5
stellaryrock <alohakii@naver.com>
2025-08-21 16:32:53 +0900

    my

--- app/my/edit.tsx
+++ app/my/edit.tsx
@@ -3,6 +3,7 @@
 import LabelInput from '@/components/ui/label-input';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
+import defaultProfile from '@/public/default-profile.jpg';
 
 export default function Edit(){
   const session = useSession();
@@ -10,19 +11,19 @@ export default function Edit(){
 
   return (  
     <>
-      <div className='flex flex-col gap-5'>
-        <LabelInput label={'이름'} defaultValue={user?.name ?? ''} />  
-        <LabelInput label={'이메일'} defaultValue={user?.email ?? ''} />
+      <form action="" className='flex flex-col gap-7'>
+        <LabelInput label={'이름'} name='name' defaultValue={user?.name ?? ''} />  
+        <LabelInput label={'이메일'} name='email' defaultValue={user?.email ?? ''} />
         <div>
           <Image
             width={50}
             height={50}
-            src={user?.image ?? 'path/to/default/image'}
+            src={user?.image ?? defaultProfile}
             alt={`${user?.name} profile image`}
           />
-          <LabelInput label={'프로필'} type="file" />
+          <LabelInput label={'프로필'} name='image' type="file" />
         </div>
-      </div>
+      </form>
     </>
   )
 }
--- app/my/page.tsx
+++ app/my/page.tsx
@@ -15,23 +15,26 @@ export default function My() {
         <h1 className='text-3xl'>
           내 정보
         </h1>
-        <button onClick={()=>{ logout(); }}>
+        <button onClick={()=>logout()}>
           로그아웃
         </button>
       </div>
-      <div className="grid grid-cols-[1fr_3fr]">
-        <div className='flex flex-col items-center'>
-          <div className='p-3'>
-            <button onClick={()=>setTabIndex(0)}>정보 수정</button>
+      <div className='flex justify-center'>
+        <div className="grid grid-cols-[1fr_3fr] w-1/2">
+          <div className='flex flex-col items-center'>
+            <div className='p-3'>
+              <button onClick={()=>setTabIndex(0)}>정보 수정</button>
+            </div>
+            <div className='p-3'>
+              <button onClick={()=>setTabIndex(1)}>회원 탈퇴</button>
+            </div>
           </div>
-          <div className='p-3'>
-            <button onClick={()=>setTabIndex(1)}>회원 탈퇴</button>
+          <div className='my-5'>
+            {tabIndex ? <Withdrawl /> : <Edit />}
           </div>
-        </div>
-        <div className='my-5'>
-          {tabIndex ? <Withdrawl /> : <Edit />}
         </div>
       </div>
+      
     </div>    
   );
 }
