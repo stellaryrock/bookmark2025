e4dd9884569634cc17f4f10d3cf2fe5dc5d7dbf5
stellaryrock <alohakii@naver.com>
2025-08-25 20:30:20 +0900

    rebase & merge

--- lib/auth.ts
+++ lib/auth.ts
@@ -138,5 +138,9 @@ export const {
       }
       return session;
     },
+    async redirect({ url, baseUrl }) {
+      console.log("redirect:", baseUrl, url);
+      return baseUrl;
+    },
   },
 });
